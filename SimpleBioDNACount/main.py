3bdac67abfcd5189bca3d8e96e01be2dbf11ffa4
phrbmr <73762126+phrbmr@users.noreply.github.com>
2021-09-07 22:42:22 +0000

    Added Simple Bioinformatics DNA Count.

---
+++ SimpleBioDNACount/main.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+
+##Input Text Box
+
+#st.sidebar.header('Enter DNA Sequence')
+st.header('Enter DNA Sequence')
+
+sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+
+#sequence = st.sidebar.text_area("Sequence input", sequence_input, height=250)
+sequence = st.text_area("Sequence input", sequence_input, height=200)
+sequence = sequence.splitlines()
+sequence = sequence[1:]      #skips the sequence name in the first line
+sequence = ''.join(sequence) #concatenates list to string
+
+st.write("""
+***
+""")
+
+##Prints the input DNA Sequence
+
+st.header('INPUT (DNA Query)')
+sequence
+
+## DNA nucleotide
+st.header('OUTPUT (DNA Nucleotide Count)')
+
+###1. Print Dictionary
+st.subheader('1. Print Dictionary')
+def DNA_nucleotide_count(seq):
+  d = dict([
+            ('A',seq.count('A')),
+            ('T',seq.count('T')),
+            ('G',seq.count('G')),
+            ('C',seq.count('C'))
+            ])
+  return d
+
+X = DNA_nucleotide_count(sequence)
+
+#X_label = list(X)
+#X_values = list(X.values())
+
+X
+
+###2. Print text
+
+st.subheader('2. Print text')
+st.write('There are  ' + str(X['A']) + ' adenine (A)')
+st.write('There are  ' + str(X['T']) + ' thymine (T)')
+st.write('There are  ' + str(X['G']) + ' guanine (G)')
+st.write('There are  ' + str(X['C']) + ' cytosine (C)')
+
+### 3. Display DataFrame
+
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'nucleotide'})
+st.write(df)
+
+### 4. Display Bar Chart using Altair
+
+st.subheader('4. Display Bar chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80)  # controls width of bar.
+)
+st.write(p)
+
+#to view the app on a browser run:
+#streamlit run main.py
